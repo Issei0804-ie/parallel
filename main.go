dc236cae51a3278770298b3a580d1fc899479e55
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-08-10 23:41:25 +0900

    update

--- main.go
+++ main.go
@@ -45,8 +45,8 @@ func main() {
 		})
 	}
 
-	//w, v, b := solve(items, weights)
-	w, v, b := parallelSolve(items, weights)
+	w, v, b := solve(items, weights)
+	//w, v, b := parallelSolve(items, weights)
 	fmt.Printf("w = %v v = %v b = %v", w, v, b)
 }
 
@@ -56,12 +56,20 @@ func solve(items []Item, weight uint64) (w, v, bits uint64) {
 	w = 0
 	v = 0
 	bits = 0
+	/*
+		for j = 0; j < uint64(p); j++ {
+			wg.Add(1)
+			go parallelCalc(items, j, uint64(p), weight, &wg, &mu)
+		}
+	*/
 	for j = 0; j < uint64(p); j++ {
 		w2, v2 := calcWeight(items, j)
-		if w2 <= weight && w < w2 {
-			w = w2
-			v = v2
-			bits = j
+		if w2 <= weight {
+			if v < v2 {
+				w = w2
+				v = v2
+				bits = j
+			}
 		}
 	}
 	return
@@ -71,6 +79,9 @@ func calcWeight(items []Item, bits uint64) (weight, value uint64) {
 	weight = 0
 	value = 0
 	for i := 0; i < len(items); i++ {
+		if bits < 0 {
+			return
+		}
 		if bits&1 == 1 {
 			weight += items[i].Weight
 			value += items[i].Value
@@ -87,6 +98,7 @@ type Result struct {
 func parallelSolve(items []Item, weight uint64) (w, v, bits uint64) {
 	var j uint64
 	p := math.Pow(2, float64(len(items)))
+	log.Println(p)
 	w = 0
 	v = 0
 	bits = 0
@@ -94,40 +106,30 @@ func parallelSolve(items []Item, weight uint64) (w, v, bits uint64) {
 	wg := sync.WaitGroup{}
 	mu := sync.Mutex{}
 
+	answer.w = 0
+	answer.v = 0
+	answer.b = 0
 	for j = 0; j < uint64(p); j++ {
 		wg.Add(1)
-		go parallelCalcWeight(items, j, weight, &wg, &mu)
+		go parallelCalc(items, j, uint64(p), weight, &wg, &mu)
 	}
 	wg.Wait()
 
+	mu.Lock()
 	w = answer.w
 	v = answer.v
 	bits = answer.b
-	/*
-		for i = 0; i < uint64(p); i++ {
-			result := <-r
-			if result.isOver {
-				continue
-			}
-			w2 := result.w
-			v2 := result.v
-			b2 := result.b
-			if w2 <= weight && w < w2 {
-				w = w2
-				v = v2
-				bits = b2
-			}
-		}
-	*/
+	mu.Unlock()
+
 	return
 }
 
-func parallelCalcWeight(items []Item, bits, weights uint64, wg *sync.WaitGroup, mu *sync.Mutex) {
-	var weight, value uint64
+func parallelCalc(items []Item, bits, p, weights uint64, wg *sync.WaitGroup, mu *sync.Mutex) {
+	var weight, value, i uint64
 	weight = 0
 	value = 0
 	shiftBits := bits
-	for i := 0; i < len(items); i++ {
+	for i = 0; i < p; i++ {
 		if shiftBits&1 == 1 {
 			weight += items[i].Weight
 			value += items[i].Value
