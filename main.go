3a8aab613a48138c87675882798728437dbf1b2a
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-08-10 17:58:18 +0900

    並列処理化

--- main.go
+++ main.go
@@ -7,12 +7,22 @@ import (
 )
 
 type Item struct {
-	Value  int
-	Weight int
+	Value  uint64
+	Weight uint64
 }
 
+/*
+6 65
+120 10
+130 12
+80 7
+100 9
+250 21
+185 16
+*/
+
 func main() {
-	var n, weights int
+	var n, weights, i uint64
 	_, err := fmt.Scanf("%v %v", &n, &weights)
 	if err != nil {
 		log.Fatalf(err.Error())
@@ -20,8 +30,8 @@ func main() {
 
 	var items []Item
 
-	for i := 0; i < n; i++ {
-		var value, weight int
+	for i = 0; i < n; i++ {
+		var value, weight uint64
 		_, err = fmt.Scanf("%v %v", &value, &weight)
 		if err != nil {
 			log.Fatalf(err.Error())
@@ -32,18 +42,18 @@ func main() {
 		})
 	}
 
-	w, v, b := solve(items, weights)
+	//w, v, b := solve(items, weights)
+	w, v, b := parallelSolve(items, weights)
 	fmt.Printf("w = %v v = %v b = %v", w, v, b)
 }
 
-func solve(items []Item, weight int) (w, v, bits int) {
-	fmt.Println(items)
+func solve(items []Item, weight uint64) (w, v, bits uint64) {
+	var j uint64
 	p := math.Pow(2, float64(len(items)))
 	w = 0
 	v = 0
 	bits = 0
-	for j := 0; j < int(p); j++ {
-		fmt.Println(j)
+	for j = 0; j < uint64(p); j++ {
 		w2, v2 := calcWeight(items, j)
 		if w2 <= weight && w < w2 {
 			w = w2
@@ -54,7 +64,7 @@ func solve(items []Item, weight int) (w, v, bits int) {
 	return
 }
 
-func calcWeight(items []Item, bits int) (weight, value int) {
+func calcWeight(items []Item, bits uint64) (weight, value uint64) {
 	weight = 0
 	value = 0
 	for i := 0; i < len(items); i++ {
@@ -65,4 +75,56 @@ func calcWeight(items []Item, bits int) (weight, value int) {
 		bits = bits >> 1
 	}
 	return
+}
+
+type Result struct {
+	w, v, b uint64
+	isOver  bool
+}
+
+func parallelSolve(items []Item, weight uint64) (w, v, bits uint64) {
+	var j uint64
+	p := math.Pow(2, float64(len(items)))
+	w = 0
+	v = 0
+	bits = 0
+
+	r := make(chan Result)
+
+	for j = 0; j < uint64(p); j++ {
+		go parallelCalcWeight(items, j, r)
+	}
+
+	for j = 0; j < uint64(p); j++ {
+		result := <-r
+		w2 := result.w
+		v2 := result.v
+		b2 := result.b
+		if w2 <= weight && w < w2 {
+			w = w2
+			v = v2
+			bits = b2
+		}
+	}
+	return
+}
+
+func parallelCalcWeight(items []Item, bits uint64, r chan Result) {
+	var weight, value uint64
+	weight = 0
+	value = 0
+	shiftBits := bits
+	result := Result{}
+	for i := 0; i < len(items); i++ {
+		if shiftBits&1 == 1 {
+			weight += items[i].Weight
+			value += items[i].Value
+		}
+		shiftBits = shiftBits >> 1
+	}
+	result.w = weight
+	result.v = value
+	result.b = bits
+
+	r <- result
 }
--- main_test.go
+++ main_test.go
@@ -4,25 +4,268 @@ import "testing"
 
 func Test_solve(t *testing.T) {
 	type args struct {
-		i       interface{}
-		weights int
+		items  []Item
+		weight uint64
 	}
 	tests := []struct {
-		name string
-		args args
+		name     string
+		args     args
+		wantW    uint64
+		wantV    uint64
+		wantBits uint64
 	}{
 		// TODO: Add test cases.
-		{"test", args{
-			i: map[int]map[int]int{
-				0: {1: 1},
-				1: {1: 1},
+		{
+			name: "n=10 & solve",
+			args: args{
+				items: []Item{
+					{Value: 3, Weight: 15},
+					{Value: 7, Weight: 3},
+					{Value: 3, Weight: 12},
+					{Value: 10, Weight: 26},
+					{Value: 6, Weight: 14},
+					{Value: 3, Weight: 21},
+					{Value: 4, Weight: 6},
+					{Value: 4, Weight: 18},
+					{Value: 6, Weight: 13},
+					{Value: 9, Weight: 3},
+				},
+				weight: 50,
 			},
-
-			weights: 0,
-		}},
+			wantW:    50,
+			wantV:    20,
+			wantBits: 42,
+		},
+		{
+			name: "n=20 & solve",
+			args: args{
+				items: []Item{
+					{Value: 6, Weight: 11},
+					{Value: 3, Weight: 28},
+					{Value: 7, Weight: 6},
+					{Value: 4, Weight: 26},
+					{Value: 9, Weight: 3},
+					{Value: 6, Weight: 11},
+					{Value: 5, Weight: 10},
+					{Value: 5, Weight: 30},
+					{Value: 3, Weight: 25},
+					{Value: 5, Weight: 29},
+					{Value: 4, Weight: 16},
+					{Value: 10, Weight: 27},
+					{Value: 4, Weight: 25},
+					{Value: 7, Weight: 15},
+					{Value: 3, Weight: 24},
+					{Value: 9, Weight: 30},
+					{Value: 4, Weight: 13},
+					{Value: 5, Weight: 7},
+					{Value: 8, Weight: 15},
+					{Value: 3, Weight: 30},
+				},
+				weight: 60,
+			},
+			wantW:    60,
+			wantV:    14,
+			wantBits: 14,
+		},
+		{
+			name: "n=40 & solve",
+			args: args{
+				items: []Item{
+					{Value: 3, Weight: 10},
+					{Value: 8, Weight: 17},
+					{Value: 5, Weight: 26},
+					{Value: 6, Weight: 10},
+					{Value: 10, Weight: 13},
+					{Value: 7, Weight: 3},
+					{Value: 8, Weight: 29},
+					{Value: 9, Weight: 18},
+					{Value: 5, Weight: 18},
+					{Value: 7, Weight: 14},
+					{Value: 3, Weight: 14},
+					{Value: 8, Weight: 29},
+					{Value: 7, Weight: 25},
+					{Value: 6, Weight: 23},
+					{Value: 4, Weight: 19},
+					{Value: 4, Weight: 14},
+					{Value: 10, Weight: 4},
+					{Value: 7, Weight: 16},
+					{Value: 9, Weight: 22},
+					{Value: 10, Weight: 23},
+					{Value: 3, Weight: 18},
+					{Value: 3, Weight: 17},
+					{Value: 4, Weight: 5},
+					{Value: 5, Weight: 5},
+					{Value: 9, Weight: 9},
+					{Value: 3, Weight: 16},
+					{Value: 7, Weight: 8},
+					{Value: 6, Weight: 17},
+					{Value: 10, Weight: 16},
+					{Value: 9, Weight: 21},
+					{Value: 10, Weight: 20},
+					{Value: 10, Weight: 3},
+					{Value: 3, Weight: 26},
+					{Value: 9, Weight: 8},
+					{Value: 4, Weight: 5},
+					{Value: 4, Weight: 30},
+					{Value: 8, Weight: 22},
+					{Value: 6, Weight: 3},
+					{Value: 8, Weight: 5},
+					{Value: 9, Weight: 18},
+				},
+				weight: 500,
+			},
+			wantW:    0,
+			wantV:    0,
+			wantBits: 0,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			gotW, gotV, gotBits := solve(tt.args.items, tt.args.weight)
+
+			if gotW != tt.wantW {
+				t.Errorf("solve() gotW = %v, want %v", gotW, tt.wantW)
+			}
+			if gotV != tt.wantV {
+				t.Errorf("solve() gotV = %v, want %v", gotV, tt.wantV)
+			}
+			if gotBits != tt.wantBits {
+				t.Errorf("solve() gotBits = %v, want %v", gotBits, tt.wantBits)
+			}
+		})
+	}
+}
+
+func Test_parallelSolve(t *testing.T) {
+	type args struct {
+		items  []Item
+		weight uint64
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantW    uint64
+		wantV    uint64
+		wantBits uint64
+	}{
+		// TODO: Add test cases.
+		{
+			name: "n=10 & parallelSolve",
+			args: args{
+				items: []Item{
+					{Value: 3, Weight: 15},
+					{Value: 7, Weight: 3},
+					{Value: 3, Weight: 12},
+					{Value: 10, Weight: 26},
+					{Value: 6, Weight: 14},
+					{Value: 3, Weight: 21},
+					{Value: 4, Weight: 6},
+					{Value: 4, Weight: 18},
+					{Value: 6, Weight: 13},
+					{Value: 9, Weight: 3},
+				},
+				weight: 50,
+			},
+			wantW:    50,
+			wantV:    20,
+			wantBits: 42,
+		},
+		{
+			name: "n=20 & parallelSolve",
+			args: args{
+				items: []Item{
+					{Value: 6, Weight: 11},
+					{Value: 3, Weight: 28},
+					{Value: 7, Weight: 6},
+					{Value: 4, Weight: 26},
+					{Value: 9, Weight: 3},
+					{Value: 6, Weight: 11},
+					{Value: 5, Weight: 10},
+					{Value: 5, Weight: 30},
+					{Value: 3, Weight: 25},
+					{Value: 5, Weight: 29},
+					{Value: 4, Weight: 16},
+					{Value: 10, Weight: 27},
+					{Value: 4, Weight: 25},
+					{Value: 7, Weight: 15},
+					{Value: 3, Weight: 24},
+					{Value: 9, Weight: 30},
+					{Value: 4, Weight: 13},
+					{Value: 5, Weight: 7},
+					{Value: 8, Weight: 15},
+					{Value: 3, Weight: 30},
+				},
+				weight: 60,
+			},
+			wantW:    60,
+			wantV:    14,
+			wantBits: 14,
+		},
+		{
+			name: "n=40 & parallelSolve",
+			args: args{
+				items: []Item{
+					{Value: 3, Weight: 10},
+					{Value: 8, Weight: 17},
+					{Value: 5, Weight: 26},
+					{Value: 6, Weight: 10},
+					{Value: 10, Weight: 13},
+					{Value: 7, Weight: 3},
+					{Value: 8, Weight: 29},
+					{Value: 9, Weight: 18},
+					{Value: 5, Weight: 18},
+					{Value: 7, Weight: 14},
+					{Value: 3, Weight: 14},
+					{Value: 8, Weight: 29},
+					{Value: 7, Weight: 25},
+					{Value: 6, Weight: 23},
+					{Value: 4, Weight: 19},
+					{Value: 4, Weight: 14},
+					{Value: 10, Weight: 4},
+					{Value: 7, Weight: 16},
+					{Value: 9, Weight: 22},
+					{Value: 10, Weight: 23},
+					{Value: 3, Weight: 18},
+					{Value: 3, Weight: 17},
+					{Value: 4, Weight: 5},
+					{Value: 5, Weight: 5},
+					{Value: 9, Weight: 9},
+					{Value: 3, Weight: 16},
+					{Value: 7, Weight: 8},
+					{Value: 6, Weight: 17},
+					{Value: 10, Weight: 16},
+					{Value: 9, Weight: 21},
+					{Value: 10, Weight: 20},
+					{Value: 10, Weight: 3},
+					{Value: 3, Weight: 26},
+					{Value: 9, Weight: 8},
+					{Value: 4, Weight: 5},
+					{Value: 4, Weight: 30},
+					{Value: 8, Weight: 22},
+					{Value: 6, Weight: 3},
+					{Value: 8, Weight: 5},
+					{Value: 9, Weight: 18},
+				},
+				weight: 500,
+			},
+			wantW:    0,
+			wantV:    0,
+			wantBits: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotW, gotV, gotBits := parallelSolve(tt.args.items, tt.args.weight)
+
+			if gotW != tt.wantW {
+				t.Errorf("parallelSolve() gotW = %v, want %v", gotW, tt.wantW)
+			}
+			if gotV != tt.wantV {
+				t.Errorf("parallelSolve() gotV = %v, want %v", gotV, tt.wantV)
+			}
+			if gotBits != tt.wantBits {
+				t.Errorf("parallelSolve() gotBits = %v, want %v", gotBits, tt.wantBits)
+			}
 		})
 	}
 }
